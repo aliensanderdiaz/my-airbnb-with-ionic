8a134baef58309030fdb89a51bb160b0436f4380
aliensanderdiaz <aliensanderdiaz@gmail.com>
2023-08-09 18:11:16 -0500

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'my-airbnb',
+  appId: 'com.aliensanderdiaz.myairbnb',
+  appName: 'My Airbnb',
   webDir: 'www',
   server: {
     androidScheme: 'https'
